57d55edfc1b15de1186d6a8234803853ade6047c
Akira13641 <operator97@gmail.com>
2020-02-19 09:14:28 -0500

    Pass the example-specific window title directly
    Also make some minor formatting adjustments

--- Examples/00-helloworld/helloworld.pas
+++ Examples/00-helloworld/helloworld.pas
@@ -37,15 +37,25 @@ begin
     'Color can be changed with ANSI ' + #27'[9;me'#27'[10;ms'#27'[11;mc'#27'[12;ma'#27'[13;mp'#27'[14;me'#27'[0m' + ' codes too.'
   );
 
-  bgfx_dbg_text_printf(80, 1, $0f, #27'[;0m    '#27'[;1m    '#27'[; 2m    '#27'[; 3m    '#27'[; 4m    '#27'[; 5m    '#27'[; 6m    '#27'[; 7m    '#27'[0m');
-  bgfx_dbg_text_printf(80, 2, $0f, #27'[;8m    '#27'[;9m    '#27'[;10m    '#27'[;11m    '#27'[;12m    '#27'[;13m    '#27'[;14m    '#27'[;15m    '#27'[0m');
+  bgfx_dbg_text_printf(
+    80,
+    1,
+    $0f,
+    #27'[;0m    '#27'[;1m    '#27'[; 2m    '#27'[; 3m    '#27'[; 4m    '#27'[; 5m    '#27'[; 6m    '#27'[; 7m    '#27'[0m'
+  );
+  bgfx_dbg_text_printf(
+    80,
+    2,
+    $0f,
+    #27'[;8m    '#27'[;9m    '#27'[;10m    '#27'[;11m    '#27'[;12m    '#27'[;13m    '#27'[;14m    '#27'[;15m    '#27'[0m'
+  );
 
   bgfx_dbg_text_printf(0, 3, $1f, 'PasBGFX/Examples/00-helloworld');
   bgfx_dbg_text_printf(0, 4, $3f, 'Description: Initialization and debug text with the Pascal API.');
 end;
 
 begin
-  TExampleRunner.Initialize();
+  TExampleRunner.Initialize('PasBGFX Hello World');
   TExampleRunner.Run(@Render);
   TExampleRunner.Finalize();
 end.
